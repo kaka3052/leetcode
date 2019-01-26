0e5dd3d91c24ef40aabeaa8f068555e460e1ef39
heyan <heyan305@gmail.com>
2019-01-26 20:09:19 +0800

    5

--- lc04-25-reverse-nodes-in-k-group.java
+++ lc04-25-reverse-nodes-in-k-group.java
@@ -28,6 +28,38 @@
  */
 class Solution {
     public ListNode reverseKGroup(ListNode head, int k) {
+        return reverseGroup(null, head, k); 
+    }
+    
+    public ListNode reverseGroup(ListNode prevFromLastGroup, ListNode head, int k){
+        if (head == null){
+            return null;
+        }
+        ListNode prev = prevFromLastGroup;
+        ListNode cur = head;
+        
+        int i = 1;
+        
+        while(i <= k && cur != null){
+            if (cur.next != null){
+                if (i == k){//the kth node
+                    head.next = cur.next;
+                    reverseGroup(cur, cur.next, k);
+                    return cur;
+                }else{
+                    ListNode next = cur.next;  
+                    cur.next = prev;
+                    prev = cur;
+                    cur = next;              
+                    i++;    
+                }
+            }              
+        }
+        if (i < k){
+            return reverseGroup(prevFromLastGroup, prev, i);
+        }else{
+            return null;
+        }
         
     }
 }
